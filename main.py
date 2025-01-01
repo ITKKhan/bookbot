1f953ec8264940e437537f445f0e9790be961b10
ITKKhan <aargmn@gmail.com>
2025-01-01 02:15:25 -0500

    Updated main to count words

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
-main()  
+file_info = main()
+
+def count_words(file_info):
+    num_words = len(file_info.split())    
+    return num_words  
+
+count = count_words(file_info)
+print(count)
