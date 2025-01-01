b8e9971eb9dc5e2937e3b599de75f5a4e02dba53
ITKKhan <aargmn@gmail.com>
2025-01-01 14:57:31 -0500

    Updated main with report print out

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        return file_contents
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    char_count = count_char(text)
+    print(print_report(book_path,num_words,char_count))
 
-file_info = main()
+    
 
-def count_words(file_info):
-    num_words = len(file_info.split())    
-    return num_words  
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-count = count_words(file_info)
-print(count)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_char(string):
+    chars = {}
+    char_list = []
+    string = string.lower()
+    for char in string:
+        if char.isalpha():
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    sorted_keys = sorted(chars.keys())
+    char_list = sorted(chars.items(), key=lambda x: x[1], reverse=True)
+
+    return char_list
+
+def print_report(book, number, list):
+    print("--- Begin report of ", book, " ---")
+    print(f"{number} words found in the document")
+    for char, count in list:
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
+
+main()
